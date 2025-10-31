eb9f92fe7d7c1a897fdc8ac6a9b4b802d5b7c6e1
Angelika <angelika.mikolajczyk11@gmail.com>
2025-10-31 12:16:52 +0100

    add local notificaiton

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowBanner: true,
+      shouldShowList: true
+    }
+  }
+});
 
 export default function App() {
+   const permissionsHandler = async () => {
+    const settings = await Notifications.getPermissionsAsync();
+ 
+    const isGranted = settings.granted;
+    if (isGranted) {
+      Alert.alert(
+        "Permission has already been granted!",
+        "You can receive notifications"
+      );
+    } else {
+      const request = await Notifications.requestPermissionsAsync();
+ 
+      if (request.granted) {
+        Alert.alert(
+          "You have granted permissions",
+          "You can now receive notifications"
+        );
+      } else {
+        Alert.alert(
+          "You did not grant permissions",
+          "You will be unable to receive notifications"
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    const subscriptionNotify = Notifications.addNotificationReceivedListener((notification) => {
+      console.log('notification received');
+      console.log('notification', notification.request.content);
+    });
+
+    const subscriptionNotifyResponse = Notifications.addNotificationResponseReceivedListener((response) => {
+       console.log('notification response received');
+      console.log('response', response.notification.request.content);
+    });
+
+    return () => {
+      subscriptionNotify.remove();
+      subscriptionNotifyResponse.remove();
+    }
+  }, [])
+
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'First local notification',
+        body: 'This is the body of first notification!',
+        data: {
+          userName: 'Angelika'
+        }
+      },
+      trigger: {
+        type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+        seconds: 5,
+      }
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={styles.container}>
       <StatusBar style="auto" />
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title="Permissions"
+          onPress={permissionsHandler}
+        />
+      </View>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
     </View>
   );
 }
